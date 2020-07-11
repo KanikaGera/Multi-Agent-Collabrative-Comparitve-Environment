85af901c837eaaa8693030d18c7d322b290127a3
Ubuntu <ubuntu@ip-172-31-4-16.ap-south-1.compute.internal>
2020-07-11 03:44:48 +0000

    feat: Final Code Train.py trained eNvrionemnt Solved in 12k episodes, saved folder updated.

--- Tennis.py
+++ Tennis.py
@@ -2,7 +2,7 @@ from unityagents import UnityEnvironment
 import numpy as np
 import pickle
 
-env = UnityEnvironment(file_name="Tennis_Linux_NoVis/Tennis_x86_64")
+env = UnityEnvironment(file_name="Tennis_Linux_NoVis/Tennis.x86_64")
 
 
 brain_name = env.brain_names[0]
@@ -38,12 +38,12 @@ def ddpg(num_agents=2, n_episodes=20000, max_t=1000):
 	episode_scores = []                                    # list containing scores from each episode
 	scores_window = deque(maxlen=100)
 	high= False
-    for i_episode in range(1, n_episodes + 1):
-    	if(i_episode ==1):
+	for i_episode in range(1, n_episodes + 1):
+		if(i_episode ==1):
 			print("Training Started .. ")
 		env_info = env.reset(train_mode=True)[brain_name]  # reset the environment
 		states = env_info.vector_observations              # get the current state (for each agent)
-		states = np.reshape(states, (1, 48)) 
+		states = np.reshape(states, (1, 48))
 		for i_agent in range(num_agents):
 			agent[i_agent].reset()
 
@@ -57,15 +57,13 @@ def ddpg(num_agents=2, n_episodes=20000, max_t=1000):
 
 			env_info = env.step(actions_)[brain_name]       # send all actions to tne environment
 			next_states = env_info.vector_observations     # get next state (for each agent)
-            next_states = np.reshape(next_states, (1, 48)) 
+			next_states = np.reshape(next_states, (1, 48))
 
 			rewards = env_info.rewards                     # get reward (for each agent)
 			dones = env_info.local_done                    # see if episode finished
 			
 			for i_agent in range(num_agents):
-				agent[i_agent].step(states,
-							actions[i_agent],
-							rewards[i_agent],next_states,dones[i_agent], t)              # update the system
+				agent[i_agent].step(states,actions[i_agent],rewards[i_agent],next_states,dones[i_agent], t)              # update the system
 			score += rewards                               # update the score (for each agent)
 			states = next_states                           # roll over states to next time step
             
@@ -75,7 +73,7 @@ def ddpg(num_agents=2, n_episodes=20000, max_t=1000):
 		episode_scores.append(np.max(score))
 
 		if (i_episode%100==0):
-			print('\rEpisode {}\tAverage Score: {:.4f}\t Max Score: {:.4f}'.format(i_episode, np.mean(scores_window), np.max(score)))
+			print('\rEpisode {}\tAverage Score: {:.4f}\t Max Score: {:.4f}'.format(i_episode, np.mean(scores_window), np.max(scores_window)))
 			torch.save(agent[0].actor_local.state_dict(), 'saved/checkpoint_actor_local_1.pth')
 			torch.save(agent[0].critic_local.state_dict(), 'saved/checkpoint_critic_local_1.pth')
 			torch.save(agent[0].actor_target.state_dict(), 'saved/checkpoint_actor_target_1.pth')
